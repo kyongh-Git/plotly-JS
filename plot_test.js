684c6afe4e9faa65b6883461aa9f67cf6f8446be
kyongh-Git <55642439+kyongh-Git@users.noreply.github.com>
2021-03-29 11:31:14 -0500

    Add files via upload

--- plot_test.js
+++ plot_test.js
@@ -5,4 +5,36 @@ Plotly.plot( TESTER, [{
     x: [1, 2, 3, 4, 5],
     y: [1, 2, 4, 8, 16] }], { 
     margin: { t: 0 } }, {showSendToCloud:true} );
+	
+	
+function rand() {
+  return Math.random();
+}
+
+TESTER2 = document.getElementById('tester2');
+
+Plotly.plot(TESTER2, [{
+  y: [1,2,3].map(rand),
+  mode: 'lines+markers', 
+  marker: {color: 'pink', size: 8},
+  line: {width: 4}
+}, {
+  y: [1,2].map(rand),
+  mode: 'lines+markers',
+  marker: {color: 'gray', size:8},
+  line: {width: 4}
+}]);
+
+var cnt = 0;
+var interval = setInterval(function() {
+  Plotly.extendTraces('graph', {
+    y: [[rand()], [rand()]]
+  }, [0, 1])
+
+  cnt = cnt+1;
+  if(cnt === 100) clearInterval(interval);
+}, 500);
+
+
 });
+
