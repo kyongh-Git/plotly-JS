417364fa47d3d91f6f37989822920ec20ae0b145
kyongh-Git <55642439+kyongh-Git@users.noreply.github.com>
2021-04-14 11:56:31 -0500

    Add files via upload

--- plot_test.js
+++ plot_test.js
@@ -13,27 +13,32 @@ function rand() {
 
 TESTER2 = document.getElementById('tester2');
 
-Plotly.plot(TESTER2, [{
-  y: [1,2,3].map(rand),
-  mode: 'lines+markers', 
-  marker: {color: 'pink', size: 8},
-  line: {width: 4}
-}, {
-  y: [1,2].map(rand),
-  mode: 'lines+markers',
-  marker: {color: 'gray', size:8},
-  line: {width: 4}
-}]);
+Plotly.d3.csv('https://lcl.okstate.edu/iss/site-files/issdata.csv', function(err, rows){
+      function unpack(rows, key) {
+          return rows.map(function(row) { return row[key]; });
+      }
 
-var cnt = 0;
-var interval = setInterval(function() {
-  Plotly.extendTraces(TESTER2, {
-    y: [[rand()], [rand()]]
-  }, [0, 1])
+    var data = [{
+        type: 'choropleth',
+        locationmode: 'country names',
+        locations: unpack(rows, 'location'),
+        z: unpack(rows, 'Students'),
+        text: unpack(rows, 'location'),
+        autocolorscale: true
+    }];
 
-  cnt = cnt+1;
-  if(cnt === 100) clearInterval(interval);
-}, 500);
+    var layout = {
+      title: 'Test Title',
+      geo: {
+          projection: {
+              type: 'robinson'
+          }
+      }
+    };
+
+    Plotly.newPlot(tester2, data, layout, {showLink: false});
+
+});
 
 TESTER3 = document.getElementById('tester3');
 
@@ -49,6 +54,52 @@ var layout = {
 };
 
 Plotly.newPlot(TESTER3, data, layout);
+
+TESTER4 = document.getElementById('tester4');
+Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv', function(err, rows){
+      function unpack(rows, key) {
+          return rows.map(function(row) { return row[key]; });
+      }
+
+       var data = [{
+            type: 'choropleth',
+            locations: unpack(rows, 'CODE'),
+            z: unpack(rows, 'GDP (BILLIONS)'),
+            text: unpack(rows, 'COUNTRY'),
+            colorscale: [
+                [0,'rgb(5, 10, 172)'],[0.35,'rgb(40, 60, 190)'],
+                [0.5,'rgb(70, 100, 245)'], [0.6,'rgb(90, 120, 245)'],
+                [0.7,'rgb(106, 137, 247)'],[1,'rgb(220, 220, 220)']],
+            autocolorscale: false,
+            reversescale: true,
+            marker: {
+                line: {
+                    color: 'rgb(180,180,180)',
+                    width: 0.5
+                }
+            },
+            tick0: 0,
+            zmin: 0,
+            dtick: 1000,
+            colorbar: {
+                autotic: false,
+                tickprefix: '$',
+                title: 'GDP<br>Billions US$'
+            }
+      }];
+
+      var layout = {
+          title: '2014 Global GDP<br>Source:',
+          geo:{
+              showframe: false,
+              showcoastlines: false,
+              projection:{
+                  type: 'mercator'
+              }
+          }
+      };
+      Plotly.newPlot(TESTER4, data, layout, {showLink: false});
+});
 
 
 });
